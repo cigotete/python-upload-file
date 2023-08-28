7225c23438d291c06777aa74d114bdbf9dd1d00b
Camilo Figueroa <cigotete@gmail.com>
2023-08-28 17:39:16 -0500

    Uploading file validating file filed, file selected and secure name.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
@@ -16,11 +17,17 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+  if 'file' not in request.files:
+      return jsonify({"error": "No file part"}), 400
 
   file = request.files['file']
 
+  if file.filename == '':
+      return jsonify({"error": "No selected file"}), 400
+
   if file and allowed_file(file.filename):
-      filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+      filename = secure_filename(file.filename)
+      filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
       file.save(filepath)
       return jsonify({"message": "File uploaded successfully"}), 200
 
